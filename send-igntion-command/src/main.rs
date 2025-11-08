a3c78c5686ea290f19e06d9d29c360ea0b9215b9
Cyrill Bucher <indeximal@gmail.com>
2025-11-08 14:58:44 +0100

    implement the telemetry sending for ignition

--- send-igntion-command/src/main.rs
+++ send-igntion-command/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use protocols::{api::TelemetryCommandMessage, client::MessageSender};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sender = MessageSender::<TelemetryCommandMessage>::connect().await?;
+
+    sender.send(&TelemetryCommandMessage::StartIgntion).await?;
+
+    Ok(())
 }
